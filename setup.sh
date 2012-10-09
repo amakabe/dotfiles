408e4a18cf7b2071a43508352ebced87ee240bf2
amakabe <amakabe@livedoor.com>
2012-10-10 02:39:49 +0900

    tmuxの設定追加

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-DIR=$(cd $(dirname $0) && pwd) 
+DIR=$(cd $(dirname $0) && pwd)
+cd $DIR
+git submodule update --init
 for F in .profile .bashrc .bash_aliases .gitconfig .vim .vimrc .tmux.conf
 do
   echo "ln -s $DIR/dot$F $HOME/$F"
